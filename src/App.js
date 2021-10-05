f818f9292109db68be3190e90b960b0aff5a1708
Lydia Lima <62971275+lydialima12@users.noreply.github.com>
2021-10-05 13:34:50 -0300

    Toolbar
    Criando o componente toolbar responsivo.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
+import  Toolbar  from './componentes/toolbar/Toolbar'
 
 function App(props) {
   return (
     <div className="App">
-      <p>Olá mundo</p>
-      <Button color="danger">Danger!</Button>
+      {/* <p>Olá mundo</p> */}
+      {/* <Button color="danger">Danger!</Button> */}
+      <Toolbar />
     </div>
   );
 }
