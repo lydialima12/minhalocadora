9efa8ae5315ece55035a0955a252c38910eba203
Lydia Lima <62971275+lydialima12@users.noreply.github.com>
2021-10-20 22:09:42 -0300

    Inserindo novo componente no app.js

--- github-pages/src/App.js
+++ github-pages/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Toolbar from "./componentes/toolbar/Toolbar";
 import Footer from "./componentes/footer/Footer";
 import Routes from "./rotas/rotas";
+import Banner from "./componentes/banner/Banner";
 
 function App() {
   return (
     <div className="App">
       <Toolbar />
+      <Banner />
       <Routes />
       <Footer />
     </div>
